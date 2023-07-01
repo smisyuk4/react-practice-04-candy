b4fd732226f0e6700be01aaed9e17c444151b018
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-01 19:07:33 +0300

    Update home.js
    close fix, add user

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,15 @@
-export const Home = () => {
   const tg = window.Telegram.WebApp;
+export const Home = () => {
+
+   const user = tg.initDataUnsafe?.user,
 
   const onClose = () => {
     alert('close window')
-    tg.onClose();
+    tg.close();
   }
   return (<div>
           <main>home page</main>
+          <p>Привіт {user}</p>
           <button onClick={onClose}>Вийти</button>
     </div>)
 };
