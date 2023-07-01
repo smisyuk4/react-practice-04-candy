2d9692d123241d74a5a9b849fa88844458ea8376
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-01 18:59:29 +0300

    Update home.js
    alert

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,11 @@ export const Home = () => {
   const tg = window.Telegram.WebApp;
 
   const onClose = () => {
+    alert('close window')
     tg.onClose();
   }
   return (<div>
-          <main>home page</main>;
+          <main>home page</main>
           <button onClick={onClose}>Вийти</button>
     </div>)
 };
